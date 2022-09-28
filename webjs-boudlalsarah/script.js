a542416afa9fcfe6133879b99e7d976bdfa49156
SarahBoudlal <sarahboudlal@gmail.com>
2022-09-28 15:36:44 +0200

    modif

---
+++ webjs-boudlalsarah/script.js
@@ -0,0 +1,7 @@
+const contener = document.getElementById('conteneur');
+contener.innerHTML;
+console.log(contener); 
+const elt = document.getElementById('mon-lien')
+elt.addEventListener('click',function(){
+    elt.innerHTML ="C'est cliqué !";
+}) ;
